71a04af5222483265fc04bad3d581a38d1fda2a5
Evan Holmes <linkoo22@gmail.com>
2020-11-27 17:27:49 -0600

    move logic to server

--- server/public/sketch.js
+++ server/public/sketch.js
@@ -1,14 +1,8 @@
 const PLAYER_SIZE = 20;
 const PLAYER_SPEED = 5;
-
-// let player = {
-//     color: '#fae',
-//     x : 30,
-//     y: 50,
-//     vx: 0,
-//     vy: 0,
-//     hp: 100
-// }
+const PLAYER_SPRINT_MOD = 2;
+const WINDOW_HEIGHT = 800;
+const WINDOW_WIDTH = 800;
 
 // let players = [];
 let players = {};
@@ -16,45 +10,44 @@ let player = {};
 let socket;
 
 function setup() {
-    createCanvas(800, 800);
+    createCanvas(WINDOW_WIDTH, WINDOW_HEIGHT);
     frameRate(60);
 
     socket = io();
 
     socket.on('currentPlayers', (players) => {
         Object.keys(players).forEach( (id) => {
-            if (players[id].playerId === socket.id) {
-                addPlayer(players[id]);
-            } else {
-                addOtherPlayers(players[id]);
-            }
+            addPlayer(players[id]);
+            // if (players[id].playerId === socket.id) {
+            //     addPlayer(players[id], true);
+            // } else {
+            //     addPlayer(players[id]);
+            // }
         });
     });
 
     socket.on('newPlayer', (playerInfo) => {
-        console.log('adding a new player -- ' + playerInfo.playerId);
-        addOtherPlayers(playerInfo);
+        // console.log('adding a new player -- ' + playerInfo.playerId);
+        addPlayer(playerInfo);
     })
 
     socket.on('playerDisconnect', (playerId) => {
         Object.keys(players).forEach( (otherPlayer) => {
-            debugger;
             if (playerId === otherPlayer) {
-                // otherPlayer = null
-                // players[otherPlayer] = null;
                 delete players[otherPlayer];
             }
         })
     });
 
     socket.on('playerMoved', (playerInfo) => {
-        Object.keys(players).forEach( (playerId) => {
-            if (playerInfo.playerId === playerId) {
-                console.log(' a different player moved -- ' + playerInfo.playerId);
-                // debugger;
-                players[playerId] = playerInfo;
-            }
-        })
+        // Object.keys(players).forEach( (playerId) => {
+        //     if (playerInfo.playerId === playerId) {
+        //         // console.log(' a different player moved -- ' + playerInfo.playerId);
+        //         players[playerId] = playerInfo;
+        //     }
+        // })
+        console.log('playerMoved ', playerInfo);
+        players[playerInfo.playerId] = playerInfo;
     })
 }
 
@@ -76,15 +69,10 @@ function draw() {
     })
 }
 
-function addPlayer(playerInfo) {
-    player = playerInfo;
-    // debugger;
-    // players.push(player);
-    players[playerInfo.playerId] = playerInfo;
-}
-
-function addOtherPlayers(playerInfo) {
-    // players.push(playerInfo);
+function addPlayer(playerInfo, isCurrentPlayer = false) {
+    // if (isCurrentPlayer) {
+    //     player = playerInfo;
+    // }
     players[playerInfo.playerId] = playerInfo;
 }
 
@@ -106,28 +94,84 @@ function mouseClicked() {
     // player.y = mouseY - (PLAYER_SIZE/2);
 }
 
+// function handleKeyPress() {
+//     const oldPos = [player.x, player.y];
+//     let playerSpeed = PLAYER_SPEED;    
+
+//     if (keyIsDown(SHIFT)) {
+//         playerSpeed *= PLAYER_SPRINT_MOD;
+//     }
+
+//     if (keyIsDown(DOWN_ARROW)) {
+//         player.y += playerSpeed;
+//     }
+    
+//     if (keyIsDown(UP_ARROW)) {
+//         player.y -= playerSpeed;
+//     }
+    
+//     if (keyIsDown(RIGHT_ARROW)) {
+//         player.x += playerSpeed;
+//     }
+
+//     if (keyIsDown(LEFT_ARROW)) {
+//         player.x -= playerSpeed;
+//     }
+
+//     keepInBounds();
+
+//     if (oldPos[0] !== player.x || oldPos[1] !== player.y) {
+//         socket.emit('playerMovement', {
+//             x: player.x,
+//             y: player.y
+//         })
+//     }
+// }
+
 function handleKeyPress() {
     const oldPos = [player.x, player.y];
+    let movementData = {x: 0, y: 0}; 
+    let playerSpeed = PLAYER_SPEED;
+
+    if (keyIsDown(SHIFT)) {
+        playerSpeed *= PLAYER_SPRINT_MOD;
+    }
+
     if (keyIsDown(DOWN_ARROW)) {
-        player.y += PLAYER_SPEED;
+        movementData.y += playerSpeed;
     }
     
     if (keyIsDown(UP_ARROW)) {
-        player.y -= PLAYER_SPEED;
+        movementData.y -= playerSpeed;
     }
     
     if (keyIsDown(RIGHT_ARROW)) {
-        player.x += PLAYER_SPEED;
+        movementData.x += playerSpeed;
     }
 
     if (keyIsDown(LEFT_ARROW)) {
-        player.x -= PLAYER_SPEED;
+        movementData.x -= playerSpeed;
     }
 
-    if (oldPos[0] !== player.x || oldPos[1] !== player.y) {
-        socket.emit('playerMovement', {
-            x: player.x,
-            y: player.y
-        })
+    // keepInBounds();
+
+    if (movementData.x !== 0 || movementData.y !== 0) {
+        socket.emit('playerMovement', movementData)
+    }
+    
+}
+
+function keepInBounds() {
+    if (player.x > WINDOW_WIDTH - (PLAYER_SIZE / 2)) {
+        player.x = WINDOW_WIDTH - (PLAYER_SIZE / 2);
+    }
+    if (player.x < 0 - (PLAYER_SIZE / 2)) {
+        player.x = 0 - (PLAYER_SIZE / 2);
+    }
+    if (player.y > WINDOW_HEIGHT - (PLAYER_SIZE / 2)) {
+        player.y = WINDOW_HEIGHT - (PLAYER_SIZE / 2);
+    }
+    if (player.y < 0 - (PLAYER_SIZE / 2)) {
+        player.y = 0 - (PLAYER_SIZE / 2);
     }
 }
--- server/server.js
+++ server/server.js
@@ -6,10 +6,10 @@ io.listen(server);
 
 var port = process.env.PORT;
 if (port == null || port == "") {
-	port = 8082;
+	port = 5000;
 }
 
-var players = {};
+var PLAYER_LIST = {};
  
 app.use(express.static(__dirname + '/public'));
  
@@ -21,43 +21,81 @@ io.on('connection', function (socket) {
   	console.log('a user connected');
 
 	// create a new player and add it to our players object
-	players[socket.id] = {
+	PLAYER_LIST[socket.id] = {
 		playerId: socket.id,
-		color: '#fae',
-		x: Math.floor(Math.random() * 800) + 50,
-		y: Math.floor(Math.random() * 800) + 50,
+		// color: '#fae',
+		color: '#' + Math.floor(Math.random()*16777215).toString(16),
+		x: Math.floor(Math.random() * 800),
+		y: Math.floor(Math.random() * 800),
 		vx: 0,
 		vy: 0,
 		hp: 100
 	};
 
+	// console.log(players[socket.id].color);
+
 	// send the players object to the new player
-	socket.emit('currentPlayers', players);
+	socket.emit('currentPlayers', PLAYER_LIST);
 	
 	// update all other players of the new player
-	socket.broadcast.emit('newPlayer', players[socket.id]);
+	// socket.broadcast.emit('newPlayer', PLAYER_LIST[socket.id]);
+	socket.emit('newPlayer', PLAYER_LIST[socket.id]);
 
 	socket.on('disconnect', function () {
 		console.log('user disconnected');
 
 		// remove this player from our players object
-		delete players[socket.id];
+		delete PLAYER_LIST[socket.id];
 
 		// emit a message to all players to remove this player
 		io.emit('playerDisconnect', socket.id);
 	});
 
 	// when a player moves, update the player data
+	// socket.on('playerMovement', (movementData) => {
+	// 	console.log('a player moved', movementData);
+	// 	PLAYER_LIST[socket.id].x = movementData.x;
+	// 	PLAYER_LIST[socket.id].y = movementData.y;
+
+	// 	// emit a message to all players about the player that moved
+	// 	socket.broadcast.emit('playerMoved', PLAYER_LIST[socket.id]);
+	// })
+
 	socket.on('playerMovement', (movementData) => {
 		console.log('a player moved', movementData);
-		players[socket.id].x = movementData.x;
-		players[socket.id].y = movementData.y;
+		PLAYER_LIST[socket.id].x += movementData.x;
+		PLAYER_LIST[socket.id].y += movementData.y;
+
+		keepInBounds(PLAYER_LIST[socket.id]);
+
+		console.log(PLAYER_LIST[socket.id].x, PLAYER_LIST[socket.id].y)
 
 		// emit a message to all players about the player that moved
-		socket.broadcast.emit('playerMoved', players[socket.id]);
-	})
+		// socket.broadcast.emit('playerMoved', PLAYER_LIST[socket.id]);
+		io.emit('playerMoved', PLAYER_LIST[socket.id]);
+	});
+
 });
 
 server.listen(port, function () {
   	console.log(`Listening on ${server.address().port}`);
 });
+
+function keepInBounds(player) {
+	const PLAYER_SIZE = 20
+	const WINDOW_HEIGHT = 800;
+	const WINDOW_WIDTH = 800;
+
+    if (player.x > WINDOW_WIDTH - (PLAYER_SIZE / 2)) {
+        player.x = WINDOW_WIDTH - (PLAYER_SIZE / 2);
+    }
+    if (player.x < 0 - (PLAYER_SIZE / 2)) {
+        player.x = 0 - (PLAYER_SIZE / 2);
+    }
+    if (player.y > WINDOW_HEIGHT - (PLAYER_SIZE / 2)) {
+        player.y = WINDOW_HEIGHT - (PLAYER_SIZE / 2);
+    }
+    if (player.y < 0 - (PLAYER_SIZE / 2)) {
+        player.y = 0 - (PLAYER_SIZE / 2);
+    }
+}
